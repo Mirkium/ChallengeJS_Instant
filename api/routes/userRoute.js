125aab618c629987113d209e6a1aba78f3ce95d2
MentalOfCrow <145018989+MentalOfCrow@users.noreply.github.com>
2024-06-14 11:45:22 +0200

    gitignore + gittattributes

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -4,8 +4,7 @@ const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
-router.post('/register', userController.registerUser); 
+router.post('/register', userController.createUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
